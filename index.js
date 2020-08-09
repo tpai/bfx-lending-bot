3608fad9c281b67ac0ee672bda38f71292b4ebef
Tony Pai <tony77794@gmail.com>
2020-08-10 00:55:47 +0800

    Add condition for not enough message

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function autoOffer() {
       ? restMoney - 0.000001 // balance must not become 0
       : 0;
 
-  if (restMoney < 0) {
+  if (restMoney < 0 || restMoney < offer) {
     console.log('> balance is not enough');
     return;
   }
