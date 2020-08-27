5ab4165e3da63fb8ed22fbefd730ab3224d9d7f9
Tony Pai <tony77794@gmail.com>
2020-08-27 23:53:56 +0800

    Add lowest offer config

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+require("./utils/polyfill");
+
 const {
   createOfferAPI,
   getAvailableBalanceAPI,
@@ -23,10 +25,11 @@ async function cleanOffers() {
 
 async function autoOffer() {
   const symbol = process.env.SYMBOL;
-  const offer = process.env.EACH_OFFER;
   const keepMoney = process.env.KEEP_MONEY;
   const baseRate = process.env.BASE_RATE;
   const jumpRate = process.env.JUMP_RATE;
+  const offer = process.env.EACH_OFFER;
+  const lowestOffer = process.env.LOWEST_OFFER;
 
   console.log("=========================");
 
@@ -35,6 +38,7 @@ async function autoOffer() {
   console.log(`Base Rate: ${baseRate}`);
   console.log(`Jump Rate: ${jumpRate}`);
   console.log(`Offer: ${offer}`);
+  console.log(`Lowest Offer: ${lowestOffer}`);
 
   console.log("=========================");
 
@@ -53,17 +57,19 @@ async function autoOffer() {
     const currentBalance = await getAvailableBalanceAPI(symbol);
     const balance = currentBalance[0] * -1;
 
-    console.log(`Balance: ${balance}`);
+    console.log(`Balance: ${Number(balance).noExponents()}`);
     console.log(`Offer: ${offer}`);
 
     const restMoney = balance - keepMoney;
     const offerTimes = Math.floor(restMoney / offer);
     const lowOffer =
-      restMoney % offer >= 50
+      restMoney % offer >= lowestOffer
         ? (restMoney % offer) - 0.000001 // balance must not become 0
         : 0;
 
-    if (restMoney < 0 || (restMoney < 50 && restMoney < offer)) {
+    console.log(`Low Offer: ${lowOffer}`);
+
+    if (restMoney < 0 || (restMoney < lowestOffer && restMoney < offer)) {
       throw new Error("balance is not enough");
     }
 
